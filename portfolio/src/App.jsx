0a02938c442175ed70a2e45d42ec46fcfb554ae0
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-11-22 18:30:48 +0300

    add scroll to up for the page to always start at the top when navigating to a new page

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Components/Home'
-import About from './Components/About'
-import Projects from './Components/Projects'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Projects from './Components/Projects';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
-    <Router> 
-     <Routes>
-      <Route exact path='/' element={<Home/>}/>
-      <Route path='/projects' element={<Projects/>}/>
-      {/* <Route path='/contacts' element={<Contact />}/> */}
-      <Route path='/about' element={<About/>}/>
-    </Routes>
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        {/* <Route path='/contacts' element={<Contact />} /> */}
+        <Route path='/about' element={<About />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
 
